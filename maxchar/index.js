e951039c06463e71adaa0bb7d233330e549930f6
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-09 00:01:25 -0500

    Add Solution 1 to maxchar algorithm.

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-const maxChar = str => {}
+const maxChar = str => {
+  const lowerCaseStr = str.toLowerCase();
+  let tempArr = [];
+  let charTotal = 0;
+  let tempTotal = 0;
+  let mostUsedChar = '';
+
+  for (let char of lowerCaseStr.split('')) {
+    if (!tempArr.includes(char)) {
+      tempArr.push(char);
+
+      charTotal = lowerCaseStr.split(char).length - 1;
+
+      if (charTotal > tempTotal) {
+        tempTotal = charTotal;
+        mostUsedChar = char;
+      }
+    }
+  }
 
 module.exports = maxChar;
