d1c9ce51919896b946494a6bedcf9eef091efdb5
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-27 12:18:48 -0500

    Add evenNumberOfDigits directory including index and test files.

---
+++ evenNumberOfDigits/index.js
@@ -0,0 +1,11 @@
+// --- Directions
+// Given an array nums of integers, return how many of them contain an even number of digits
+// --- Examples:
+//   evenNumberOfDigits([12,345,2,6,7896]) === 2
+//   evenNumberOfDigits([555,901,482,1771]) === 1
+
+const evenNumberOfDigits = (nums) => {
+
+}
+
+module.exports = evenNumberOfDigits;
