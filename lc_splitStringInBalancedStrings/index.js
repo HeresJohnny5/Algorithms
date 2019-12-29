69e523ce9491d2bded899d24d59b05565db41ac5
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-29 18:52:56 -0500

    Add solution for splitStringInBalancedStrings.

--- lc_splitStringInBalancedStrings/index.js
+++ lc_splitStringInBalancedStrings/index.js
@@ -1,5 +1,6 @@
 // --- Directions
-// Balanced strings are those who have equal quantity of 'L' and 'R' characters. Given a balanced string, split it in the maximum amount of balanced strings. Return the maximum amount of splitted balanced strings.
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters. 
+// Given a balanced string, split it in the maximum amount of balanced strings. Return the maximum amount of splitted balanced strings.
 // --- Examples:
 // splitStringInBalancedStrings('RLRRLLRLRL') === 4
 // string can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
@@ -7,7 +8,18 @@
 // string can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
 
 const splitStringInBalancedStrings = (string) => {
+  let balancedVar = 0;
+  let total = 0;
 
+  string.split('').forEach(char => {
+    char === 'L' ? balancedVar++ : balancedVar--;
+
+    if (balancedVar === 0) {
+      total++;
+    }
+  });
+
+  return total;
 }
 
 module.exports = splitStringInBalancedStrings;
