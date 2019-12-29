c131852fc0a099051de6c09151dfc19704c9407e
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-28 22:44:34 -0500

    Add splitStringInBalancedStrings directory including index file.

---
+++ lc_splitStringInBalancedStrings/index.js
@@ -0,0 +1,13 @@
+// --- Directions
+// Balanced strings are those who have equal quantity of 'L' and 'R' characters. Given a balanced string, split it in the maximum amount of balanced strings. Return the maximum amount of splitted balanced strings.
+// --- Examples:
+// splitStringInBalancedStrings('RLRRLLRLRL') === 4
+// string can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+// splitStringInBalancedStrings('RLLLLRRRLR') === 3
+// string can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+const splitStringInBalancedStrings = (string) => {
+
+}
+
+module.exports = splitStringInBalancedStrings;
