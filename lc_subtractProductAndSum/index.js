adcf970e814823e16a6d9fffe3e635f852a54de5
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-28 21:30:36 -0500

    Add subtractProductAndSum directory and index file.

---
+++ lc_subtractProductAndSum/index.js
@@ -0,0 +1,11 @@
+// --- Directions
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits
+// --- Examples:
+//   subtractProductAndSum(234) === 15
+//   subtractProductAndSum(4421) === 21
+
+const defangedIPAddress = (nums) => {
+
+}
+
+module.exports = defangedIPAddress;
