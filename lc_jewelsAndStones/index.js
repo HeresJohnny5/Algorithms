4de49dd1f130d4ff1924e83e9acd8270d4d4061c
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-28 22:22:52 -0500

    Add jewelsAndStones directory including index file.

---
+++ lc_jewelsAndStones/index.js
@@ -0,0 +1,13 @@
+// --- Directions
+// You're given strings Jewels representing the types of stones that are jewels, and Stones representing the stones you have. 
+// Each character in Stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+// The letters in Jewels are guaranteed distinct, and all characters in Jewels and Stones are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+// --- Examples:
+//   jewelsAndStones('aA', 'aAAbbbb') === 3
+//   jewelsAndStones('z', 'ZZ') === 0
+
+const jewelsAndStones = (jewels, stones) => {
+
+}
+
+module.exports = jewelsAndStones;
