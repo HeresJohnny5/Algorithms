554db993fc8ebcecd728306a5fcfd3d601e53d0e
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-12-29 19:05:11 -0500

    Add replaceElementsWithGreatestElementOnRight directory including index file.

---
+++ lc_replaceElementsWithGreatestElementOnRight/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+// After doing so, return the array.
+// --- Examples:
+// replaceElementsWithGreatestElementOnRight([17,18,5,4,6,1])
+// Output = [18,6,6,6,1,-1]
+
+const replaceElementsWithGreatestElementOnRight = (array) => {
+
+}
+
+module.exports = replaceElementsWithGreatestElementOnRight;
